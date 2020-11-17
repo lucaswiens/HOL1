adf2e318867a243856837fc5414d2c20580b9c7f
Lucas Wiens <lucas.wiens@desy.de>
2020-11-17 12:35:20 +0100

    Add a progress bar

--- bin/main.cc
+++ bin/main.cc
@@ -10,13 +10,16 @@
 #include <HOAnalysis/HOL1/interface/Producer/MuonProducer.h>
 #include <HOAnalysis/HOL1/interface/Producer/BmtfInputProducer.h>
 
+void ProgressBar(const int &, const int &);
 
 int main(int argc, char* argv[]) {
+	std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+
 	std::string inputFile = std::string(argv[1]);
 
 	DataReader* dataReader = new DataReader(inputFile);
 
-	TFile *file = new TFile("l1Histograms.root", "RECREATE");
+	TFile *file = new TFile("L1Histograms.root", "RECREATE");
 
 	//Declare vector of Producers which will fill Histograms
 	std::vector<std::shared_ptr<BaseProducer>> producers = {
@@ -25,11 +28,19 @@ int main(int argc, char* argv[]) {
 		std::shared_ptr<BmtfInputProducer>(new BmtfInputProducer()),
 	};
 
+	int processed = 0;
+	ProgressBar(0, 0);
 	while(dataReader->Next()){
+		//ProgressBar
+		if(processed % 10000) {
+			ProgressBar(processed, processed / std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - start).count());
+		}
+
 		for (std::shared_ptr<BaseProducer> producer: producers) {
 			producer->Produce(dataReader);
 		}
 	}
+	ProgressBar(100, processed / std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - start).count());
 
 	file->Write();
 	file->Close();
@@ -39,3 +50,24 @@ int main(int argc, char* argv[]) {
 
 	return 0;
 }
+
+void ProgressBar(const int &progress, const int &rate) {
+	std::string progressBar = "[";
+
+	for (int i = 0; i < progress - 1; i++) {
+		progressBar += "·";
+	}
+	if (progress == 100) { progressBar += "·";}
+	else if (progress % 2 == 0) { progressBar += "c";}
+	else { progressBar += "C";}
+
+	for (int i = 0; i < 100 - progress; i++) {
+		progressBar += "•";
+	}
+
+	progressBar = progressBar + "] " + std::to_string(progress) + "% of Events processed at a rate of " + std::to_string(rate) + " Hz." ;
+	std::cout << "\r" << progressBar << std::flush;
+
+	if (progress == 100) std::cout << std::endl;
+}
+
--- interface/DataReader.h
+++ interface/DataReader.h
@@ -13,7 +13,6 @@ class DataReader {
 		TBranch *l1BmtfInputBranch;
 		TTreeReader *l1HoReader, *l1MuonRecoReader, *l1BmtfInputReader;
 	public:
-		//Upgrade BMTF Muon Inputs
 		//HO Variables
 		std::unique_ptr<TTreeReaderValue<unsigned int>> nHcalDetIds, nHcalQIESamples;
 		std::unique_ptr<TTreeReaderArray<int>> hcalDetIdIEta, hcalDetIdIPhi, hcalQIESample, hcalQIESampleAdc, hcalQIESampleDv, hcalQIESampleEr;
@@ -26,10 +25,13 @@ class DataReader {
 		std::unique_ptr<TTreeReaderArray<int>> muonPassesSingleMuon, muonCharge;
 		std::unique_ptr<TTreeReaderArray<float>> muonE, muonEt, muonPt, muonEta, muonPhi, muonIso, muonHlt_isoDeltaR, muonHlt_deltaR, muonEtaSt1, muonPhiSt1, muonEtaSt2, muonPhiSt2;
 		std::unique_ptr<TTreeReaderArray<double>> muonMet, muonMt;
-		//BMTF Input Variables
+		//BMTF Input Variables (Drift Tube Trigger Primitive)
 		std::unique_ptr<TTreeReaderValue<int>> bmtfPhSize, bmtfThSize;
 		std::unique_ptr<TTreeReaderArray<int>> bmtfPhBx, bmtfPhWh, bmtfPhSe, bmtfPhSt, bmtfPhCode, bmtfPhTs2Tag, bmtfThBx, bmtfThWh, bmtfThSe, bmtfThSt, bmtfThTheta, bmtfThCode;
 		std::unique_ptr<TTreeReaderArray<float>> bmtfPhAng, bmtfPhBandAng;
+		//BMTF Output Muon
+		std::unique_ptr<TTreeReaderValue<unsigned short>> nTfMuon;
+		std::unique_ptr<TTreeReaderArray<short>> tfMuonHwPt, tfMuonHwEta, tfMuonHwPhi, tfMuonGlobalPhi, tfMuonHwSign, tfMuonHwSignValid, tfMuonHwQual, tfMuonLink, tfMuonProcessor, tfMuonTrackFinderType, tfMuonHwHF, tfMuonBx, tfMuonWh, tfMuonTrAdd;
 
 		DataReader(std::string);
 		~DataReader();
